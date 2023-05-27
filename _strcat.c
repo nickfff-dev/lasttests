d25001efa190fb652fc059881c52dc0619192b09
nickfff-dev <hunchographics@gmail.com>
2023-05-27 03:20:36 +0300

    newbranch

--- _strcat.c
+++ _strcat.c
@@ -10,15 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+    int i;
+    int j;
 
-    while (dest[i] != '\0') /* Initialize i before using it */
+    for (i = 0; dest[i] != '\0'; i++)
     {
         i++;
     }
 
-    while (src[j] != '\0')
+    for (j = 0; src[j] != '\0'; j++)
     {
         dest[i] = src[j];
         i++;
